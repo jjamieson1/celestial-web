f7f54c28bc98559de648ad0b7dad5e3146b3968b
jjamieson1 <jjamie@gmail.com>
2024-01-07 13:23:37 -0700

    Built the nav bar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { Image, HStack, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Text fontSize="3xl" as="b" color="black" m={2}>
+        Celestial Technologies
+        <Text fontSize="3xl" as="b" color="red">
+          .
+        </Text>
+      </Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
